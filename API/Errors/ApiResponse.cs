b4017a0663d94074e874457374bf22acfb1793be
Matei <mateiaioanei06@gmail.com>
2021-07-26 11:45:56 +0300

    End of section 11

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -4,14 +4,14 @@ namespace API.Errors
 {
     public class ApiResponse
     {
-        public ApiResponse(int statusCode, string mesage = null)
+        public ApiResponse(int statusCode, string message = null)
         {
             StatusCode = statusCode;
-            Mesage = mesage ?? GetDefaultMessageForStatusCode(statusCode);
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
         }
 
         public int StatusCode { get; set; }
-        public string Mesage { get; set; }
+        public string Message { get; set; }
         
         private string GetDefaultMessageForStatusCode(int statusCode)
         {
